daabe618a273c5b7b4bbd81564171c8bc55ddf35
sneh103 <145443932+sneh103@users.noreply.github.com>
2023-09-19 17:33:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var ajaxCall = (key, url, prompt) => {
       type: "POST",
       dataType: "json",
       data: JSON.stringify({
-        model: "text-davinci-003",
+        model: "gpt-3.5-turbo",
         prompt: prompt,
         max_tokens: 1024,
         n: 1,
